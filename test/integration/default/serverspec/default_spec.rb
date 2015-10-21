eb1cd87ab6318b3cbae675de27a97a729a6ee118
Goodsmileduck <goodsmileduck@gmail.com>
2015-10-22 02:43:11 +0800

    serverspec jenkins

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,6 @@
 require 'spec_helper'
 
 describe 'jenkins-job::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
   
   describe service('jenkins') do
     it { should be_enabled }
