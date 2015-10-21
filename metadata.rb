e24c139a5bdefaa1dfd6b66f3e9f20a6a602463a
Goodsmileduck <goodsmileduck@gmail.com>
2015-10-21 21:07:59 +0800

    berks

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures jenkins-job'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "jenkins"
+depends 'jenkins'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'jenkins::java'
+
+include_recipe 'jenkins::master'
