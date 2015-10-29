3d02a7c32ae4233068ae1b7d0101b50821350cef
Goodsmileduck <goodsmileduck@gmail.com>
2015-10-29 10:15:00 +0800

    rmp and whl builds

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,13 +10,17 @@
 include_recipe 'jenkins::java'
 include_recipe 'jenkins::master'
 include_recipe 'python'
-include_recipe "jenkins-job-builder"
 #include_recipe "python::pip"
 include_recipe 'git'
 jenkins_plugin "scm-api"
 jenkins_plugin "git-client"
 jenkins_plugin "git"
 jenkins_plugin 'job-dsl'
+
+jenkins_user 'grumpy' do
+  full_name    'Grumpy Dwarf'
+  email        'grumpy@example.com'
+end
 
 python_pip "wheel" do
   action :install
@@ -38,6 +42,16 @@ jenkins_plugin 'job-dsl' do
 	notifies :restart, 'service[jenkins]', :immediately	
 end
 
+unless node['jenkins_job_builder']['from_source']
+  python_pip 'jenkins-job-builder' do
+    version node['jenkins_job_builder']['version']
+    action :install
+  end
+else
+  python_pip node['jenkins_job_builder']['repo'] do
+    action :install
+  end
+end
 
 directory '/etc/jenkins_jobs' do
   owner node['jenkins_job_builder']['user']
@@ -57,10 +71,18 @@ template '/etc/jenkins_jobs/jenkins_jobs.ini' do
   })
 end
 
-template '/etc/jenkins_jobs/jenkins-job-builder.yaml' do
-  source "jenkins-job-builder.yaml"
+template '/etc/jenkins_jobs/jenkins-job-builder-rpm.yaml' do
+  source "jenkins-job-builder-rpm.yaml"
 end
 
-build_jenkins_job 'jenkins-job-builder' do
+build_jenkins_job 'jenkins-job-builder-rpm' do
+  job_config '/etc/jenkins_jobs/'
+end
+
+template '/etc/jenkins_jobs/jenkins-job-builder-whl.yaml' do
+  source "jenkins-job-builder-whl.yaml"
+end
+
+build_jenkins_job 'jenkins-job-builder-whl' do
   job_config '/etc/jenkins_jobs/'
 end
